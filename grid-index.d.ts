7fc37ecf99de4a376fcc0cd7a1fe6b2705309e12
bluelovers <codelovers@users.sourceforge.net>
2026-04-19 21:37:44 +0800

    build: build index

--- grid-index.d.ts
+++ grid-index.d.ts
@@ -1,6 +1,5 @@
-/**
- * Created by user on 2026/4/14.
- */
 
-import { IGridBlock } from '@/lib/utils/grid/grid-types';
-export = Array<IGridBlock>
+import { IFormatBlockKey } from '@/lib/utils/grid/grid-types';
+export = {
+	activeBuckets: IFormatBlockKey<'_'>[]
+}
